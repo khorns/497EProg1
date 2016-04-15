020f0f366cab846306af8ebdb5cc987d86040168
Sereyvathanak Khorn <khorns@students.wwu.edu>
2016-04-15 16:20:47 -0700

    Working with polynomial.

--- program1/src/prog1.java
+++ program1/src/prog1.java
@@ -7,12 +7,13 @@
  * April 14, 2016
  *
  * -train train_x.txt train_y.txt a my.model 2000 2 1
+ * train dataset1: -train data/dataset1/train_x.txt data/dataset1/train_y.txt a my.model 2500 500 1
+ * train dataset3: -train data/dataset3/train_x.txt data/dataset3/train_y.txt a my.model 2000 1 8
  */
 
 import org.apache.commons.math3.linear.Array2DRowRealMatrix;
 import org.apache.commons.math3.linear.LUDecomposition;
 import org.apache.commons.math3.linear.RealMatrix;
-
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
@@ -90,21 +91,39 @@ public class prog1 {
         int N = Integer.parseInt(args[5]);
         int D = Integer.parseInt(args[6]);
         int K = Integer.parseInt(args[7]);
+        RealMatrix xhat;
+        RealMatrix yhat;
 
-        RealMatrix xhat = new Array2DRowRealMatrix(new double[N][D+1]);
-        RealMatrix yhat = new Array2DRowRealMatrix(new double[N][1]);
+//        RealMatrix xhat = new Array2DRowRealMatrix(new double[N][D+1]);
+//        RealMatrix yhat = new Array2DRowRealMatrix(new double[N][1]);
 
 
         String train_x_reader = fileOpen(filex);
         String train_y_reader = fileOpen(filey);
 
+        // Windows
 //        String[] trainXString = train_x_reader.split("\r\n");
 //        String[] trainYString = train_y_reader.split("\r\n");
+        // Linux
         String[] trainXString = train_x_reader.split(" \n");
         String[] trainYString = train_y_reader.split(" \n");
 
-        matrixConvert(trainXString, xhat, D);   // xhat
-        matrixConvert(trainYString, yhat, 0);   // yhat
+//        matrixConvert(trainXString, xhat, D);   // xhat
+//        matrixConvert(trainYString, yhat, 0);   // yhat
+
+        // Create matrix for xhat and yhat
+        if (K == 1) {
+            xhat = new Array2DRowRealMatrix(new double[N][D+1]);
+            yhat = new Array2DRowRealMatrix(new double[N][1]);
+            matrixConvert(trainXString, xhat, D);   // xhat
+            matrixConvert(trainYString, yhat, 0);   // yhat
+        }
+        else {
+            xhat = new Array2DRowRealMatrix(new double[N][K+1]);
+            yhat = new Array2DRowRealMatrix(new double[N][1]);
+            matrixConvertK(trainXString, xhat, K);   // xhat
+            matrixConvert(trainYString, yhat, 0);   // yhat
+        }
 
         // Calculating BetaHat*
         RealMatrix z = xhat.transpose().multiply(xhat);
@@ -115,6 +134,30 @@ public class prog1 {
         System.out.println(Arrays.toString(z.getColumn(0)));
     }
 
+    private static void matrixConvertK(String[] data, RealMatrix MatData, int K) {
+        double doubleTemp;
+        double original;
+        int index = 0;
+        int DTemp = 0;
+
+        for (String aTrainString : data) {
+            doubleTemp = Double.parseDouble(aTrainString);
+            original = doubleTemp;
+            while (DTemp <= K) {    // Create a row matrix, eg: [1, xxx, xxx]
+                if (DTemp == 0) {
+                    MatData.setEntry(index, 0, 1);
+                }
+                else {
+                    doubleTemp = Math.pow(original, DTemp);
+                    MatData.setEntry(index, DTemp, doubleTemp);
+                }
+                DTemp++;
+            }
+            index++;
+            DTemp = 0;
+        }
+
+    }
 
     /**
      * Build matrix
